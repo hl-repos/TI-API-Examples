b88bad97d19e08e3de4159d804d34268a03e9755
Hoplite <repos@hopliteindustries.com>
2019-06-19 12:07:19 -0600

    Added exception handling to the API call
    Invoke-RestMethod interprets all HTTP/400's as application exceptions, the exception handler was added along with a user-friendly STDOUT message.

--- hoplite_ti_test.ps1
+++ hoplite_ti_test.ps1
@@ -5,14 +5,16 @@
 ## @author	acochenour@hopliteindustries.com
 ## @copyright	2014-2016 Hoplite Industries, Inc.
 ## @license	
-## @version	0.1
+## @version	0.1.3
 ## @since	v0.1
 ##
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 ## Establish the body of the access token request
 $body = @{
 	client_id='user@domain.com'
-	client_secret='API_KEY'
+	client_secret='<API-KEY>'
 	grant_type='client_credentials'
 }
 $json = $body | ConvertTo-Json
@@ -25,6 +27,12 @@ $token = $tResponse.access_token
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Authorization", "Bearer "+$token)
 
-## With the access token and HTTP header in hand, execute the API GET to see if the subject ip, 128.75.222.116, is malicious
-$aResponse = Invoke-RestMethod 'https://ti-api.hoplite.io/ip/128.75.222.116' -Method GET -Header $headers -ContentType 'application/json'
-Write-Output $aResponse
+
+
+try {
+    ## With the access token and HTTP header in hand, execute the API GET to see if the subject ip, 128.75.222.116, is malicious
+    $aResponse = Invoke-RestMethod -Uri 'https://ti-api.hoplite.io/ip/128.75.222.116' -Method GET -Headers $headers -ContentType 'application/json' 
+    Write-Output $aResponse
+} catch {
+    Write-Host "Not Found - The Indicator Does Not Appear in the Current TI Data Set"
+}
